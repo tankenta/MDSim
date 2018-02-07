f00ac61ae0511861f2b43501a48a06fbaaa473cd
tankenta <hkenta112@gmail.com>
2018-02-08 03:44:18 +0900

    Add plot scripts

---
+++ plot_tools/config.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+RESULTS_INFO = {
+        'total': {
+            'file_name': 'total_energy.csv',
+            'plot_pos': 321,
+            'type': 'scatter',
+            'label': 'total energy',
+            'style': 'r',
+            'size': 30,
+        },
+        'potential': {
+            'file_name': 'potential_energy.csv',
+            'plot_pos': 322,
+            'type': 'scatter',
+            'label': 'potential energy',
+            'style': 'g',
+            'size': 30,
+        },
+        'kinetic': {
+            'file_name': 'kinetic_energy.csv',
+            'plot_pos': 323,
+            'type': 'scatter',
+            'label': 'kinetic energy',
+            'style': 'b',
+            'size': 30,
+        },
+        'temp': {
+            'file_name': 'temperature.csv',
+            'plot_pos': 324,
+            'type': 'scatter',
+            'label': 'temperature',
+            'style': 'm',
+            'size': 30,
+        },
+        'rdf': {
+            'file_name': 'RDF.csv',
+            'plot_pos': 325,
+            'type': 'plot',
+            'label': 'RDF',
+            'style': 'r',
+        },
+        'msd': {
+            'file_name': 'MSD.csv',
+            'plot_pos': 326,
+            'type': 'scatter',
+            'label': 'MSD',
+            'style': 'c',
+            'size': 30,
+        }
+}
+
---
+++ plot_tools/plot_results.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import csv
+import os
+import sys
+
+from matplotlib import pyplot as plt
+
+from config import *
+
+def csv2result_dict(csv_path):
+    result_dict = dict()
+
+    with open(csv_path, 'r') as f:
+        reader = csv.reader(f)
+        header = next(reader)
+        result_dict['label_x'] = header[0]
+        result_dict['label_y'] = header[1]
+
+        array_x = list()
+        array_y = list()
+        for row in reader:
+            array_x.append(float(row[0]))
+            array_y.append(float(row[1]))
+        result_dict['array_x'] = array_x
+        result_dict['array_y'] = array_y
+
+    return result_dict
+
+def plot_all_results(csv_dir):
+    fig = plt.figure()
+    for key, info in RESULTS_INFO.items():
+        csv_path = os.path.join(csv_dir, info['file_name'])
+        result_dict = csv2result_dict(csv_path)
+
+        ax = fig.add_subplot(info['plot_pos'])
+        if info['type'] == 'plot':
+            ax.plot(
+                    result_dict['array_x'], result_dict['array_y'],
+                    info['style'], label=info['label'])
+        else:
+            ax.scatter(
+                    result_dict['array_x'], result_dict['array_y'],
+                    info['size'], info['style'], label=info['label'])
+        ax.set_xlabel(result_dict['label_x'])
+        ax.set_ylabel(result_dict['label_y'])
+        ax.xaxis.set_tick_params(
+                which='both', direction='in', left=True, right=True)
+        ax.yaxis.set_tick_params(
+                which='both', direction='in', left=True, right=True)
+        ax.legend()
+    fig.tight_layout()
+    plt.show()
+
+def main():
+    if len(sys.argv) < 2:
+        print('Usage: ./plot_results.py csv_dir')
+        sys.exit()
+    csv_dir = sys.argv[1]
+    plot_all_results(csv_dir)
+
+if __name__ == '__main__':
+    main()
